3e402f28c724e96ce43f25054d19e35a113218b9
AhmedAbdulla22 <AhmedAbdulla22@users.noreply.github.com>
2025-06-06 15:10:35 +0300

    solve reverse String
    dude he used character level reversal in one line!
    an impresive approach tbh

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(text,times) {
+    if(text === '' || times === 0) return '';
+    else if(times < 0) return 'ERROR';
 
+    let res = '';
+    for (let index = 0; index < times; index++) {
+        res += text;
+    }
+
+    return res;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,33 @@
-const reverseString = function() {
+const reverseString = function(words) {
+    //MY old way to solve this!!!!!!!!!!!!!!!!!
+    // if(words === '') return '';
 
+    // let splitedWords = words.split(' ');
+
+    // let reversedWords = [];
+    
+    // for (let i = 0; i < splitedWords.length; i++) {
+    //     let reversed = '';
+    //     for (let j = splitedWords[i].length - 1; j >= 0; j--) {
+    //         reversed += splitedWords[i][j];
+    //     }
+    //     reversedWords.push(reversed);
+    // }
+
+    // let result = reversedWords[reversedWords.length-1];
+    
+    // if(reversedWords.length === 1) return result;
+    
+
+    // //skip last word by adding -2 to length
+    // for (let index = reversedWords.length - 2; index >= 0; index--) {
+    //     result += ' ' + reversedWords[index];
+    // }
+
+    // return result;
+
+    //this is character level reversal
+    return words.split('').reverse().join('');
 };
 
 // Do not edit below this line
