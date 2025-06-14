d20cd5ea5c7ab09844b2b9209ee81d28f36265af
AhmedAbdulla22 <AhmedAbdulla22@users.noreply.github.com>
2025-06-14 16:00:00 +0300

    solve removeFromArray by and deal with multi-args

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    for(const arg of args)
+    {
+        do {
+            const indexOfItem = array.indexOf(arg);
+            if(indexOfItem >= 0) {
+                array.splice(indexOfItem,1); 
+            } else {
+                break;
+            }
+        } while(array.indexOf(arg) >= 0);//check if there is other similar item left
+    }
+    
+    
+    return array;
 };
+
+//advanced solution from the solution
+// function removeFromArray(array, ...args)  {
+//     return array.filter(val => !args.includes(val))
+// }
 
 // Do not edit below this line
 module.exports = removeFromArray;
