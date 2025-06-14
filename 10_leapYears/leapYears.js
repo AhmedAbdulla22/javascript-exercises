dcd40314efd6ab9b0ca568fee6c1f048567535a9
AhmedAbdulla22 <AhmedAbdulla22@users.noreply.github.com>
2025-06-14 17:11:49 +0300

    solve leap year with readable solution

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // return ((year % 100 === 0 && year % 400 === 0 ) || year % 4 === 0 && year % 100 !== 0)
+    const isDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isDivisibleBy400 = year % 400 === 0;
 
+    if(isDivisibleByFour && (!isCentury || isDivisibleBy400)) return true;
+    else return false;
 };
 
 // Do not edit below this line
