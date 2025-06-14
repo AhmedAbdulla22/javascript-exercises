50bfc7e53c913d601179dbc6c41bcef2842b44af
AhmedAbdulla22 <AhmedAbdulla22@users.noreply.github.com>
2025-06-14 17:37:04 +0300

    solve tempCovnersion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  const Celsius = (Fahrenheit - 32) * (5/9);
+  return (Number.isInteger(Celsius)) ? Celsius : Number(Celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  const Fahrenheit = (Celsius * (9/5)) + 32; 
+  return (Number.isInteger(Fahrenheit)) ? Fahrenheit :Number(Fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
